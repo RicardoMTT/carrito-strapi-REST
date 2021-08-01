ff1ce8a38a5931e001bc61ff40951de42e6bdb8f
Ricardo Tovar <ricardotovar.grupobruild@gmail.com>
2021-08-01 00:16:23 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c9b83e5062c3e78dc60994b6f70509c0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '05632e10-413a-4009-8d00-2152dd11d478'
+};
